e90ea7b9da6dab59033a5f1bc76b4ab300cfc47b
Dr0ptp4kt <abaso@wikimedia.org>
2015-03-20 04:30:33 -0700

    Strings and connection handling

--- app/src/main/java/parksupplyofamerica/com/parksupply/MainActivity.java
+++ app/src/main/java/parksupplyofamerica/com/parksupply/MainActivity.java
@@ -1,5 +1,8 @@
 package parksupplyofamerica.com.parksupply;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
@@ -17,6 +20,10 @@ public class MainActivity extends ActionBarActivity {
     private WebView mWebView;
     private MenuItem back;
     private MenuItem forward;
+    private boolean hasErrorState = false;
+    private boolean hasVisitedFirstPage = false;
+    private Context ctx = this;
+    private static final String startingPage = "http://www.parksupplyofamerica.com/mobet.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +43,7 @@ public class MainActivity extends ActionBarActivity {
 
         mWebView.setInitialScale(80);
         mWebView.setWebViewClient(new ParkWebViewClient());
-        mWebView.loadUrl("http://www.parksupplyofamerica.com/mobet.php");
+        mWebView.loadUrl(startingPage);
     }
 
     @Override
@@ -88,7 +95,7 @@ public class MainActivity extends ActionBarActivity {
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             String host = Uri.parse(url).getHost();
-            if(host.equals("parksupplyofamerica.com") || host.endsWith(".parksupplyofamerica.com")) {
+            if(host == null || host.equals("parksupplyofamerica.com") || host.endsWith(".parksupplyofamerica.com")) {
                 return false;
             }
 
@@ -99,12 +106,33 @@ public class MainActivity extends ActionBarActivity {
 
         @Override
         public void onPageFinished(WebView view, String url) {
+            final WebView wv = view;
+            if (hasErrorState && !hasVisitedFirstPage) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(ctx);
+                builder.setMessage(R.string.conn)
+                        .setPositiveButton(R.string.retry, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                hasErrorState = false;
+                                wv.reload();
+                            }
+                        });
+                builder.create();
+                builder.show();
+                return;
+
+            }
+            hasErrorState = false;
+            hasVisitedFirstPage = true;
             refreshButtons();
         }
 
         @Override
         public void onReceivedError(WebView view, int errorCode,
                                     String description, String failingUrl) {
+            if (!hasVisitedFirstPage) {
+                hasErrorState = true;
+                return;
+            }
             view.loadData("<html><body><h1 style='text-align:center'>Connection problem :(</h1></body></html>", "text/html", "UTF-8");
             refreshButtons();
         }
